fcf0d900cfe6139c685104ca02a9b621274a2e4f
shadowSlaher <sahilrana6727@gmail.com>
2025-05-21 13:04:55 +0530

    initial03

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     # path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
     # path('products/<int:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
-    path('products/', views.product_list_create_view, name='product-list-create'),
+    path('', views.product_list_create_view, name='product-list-create'),
     path('products/<int:pk>/', views.product_detail_view, name='product-detail'),
 
 ]
